6aafc6cf2faf66265dd1168218ed98a4021a7113
Nate Ensign <dixienate@gmail.com>
2024-12-28 15:21:53 -0700

    added functionality to have dummy data of user, pic and places displayed

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return (
-    <div>Users</div>
-  )
-}
+  const USERS = [
+    {
+      id: "u1",
+      name: "Nate Ensign",
+      image:
+        "https://images.unsplash.com/photo-1508317469940-e3de49ba902e?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y3NzfGVufDB8fDB8fHww",
+      places: 3,
+    },
+  ];
 
-export default Users
+  return <UsersList items={USERS} />;
+};
+
+export default Users;
