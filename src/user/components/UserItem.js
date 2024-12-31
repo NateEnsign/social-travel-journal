972b64407aa883260dc05a5bc2c6840e6160e9b2
Nate Ensign <dixienate@gmail.com>
2024-12-31 10:04:23 -0700

    fixed stying issue for UserItem.js

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./UserItem.css";
 import Avatar from "../../shared/components/UIElements/Avatar";
 import Card from "../../shared/components/UIElements/Card";
+import "./UserItem.css";
+
 
 const UserItem = (props) => {
   return (
     <li className="user-item">
-      <Card className='user-item__content'>
+      <Card className="user-item__content" >
         <Link to={`/${props.id}/places`}>
           <div className="user-item__image">
             <Avatar image={props.image} alt={props.name} />
@@ -25,3 +26,5 @@ const UserItem = (props) => {
 };
 
 export default UserItem;
+
+
