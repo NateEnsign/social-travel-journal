5414c2d527750bf31d5ab375cc2bccaa2aff48d7
Nate Ensign <dixienate@gmail.com>
2025-01-03 09:43:46 -0700

    implemented useParams such that only a specific creatos locations will be shown when that user is clicked on

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,38 +1,42 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
 
-import PlaceList from '../components/PlaceList';
+import PlaceList from "../components/PlaceList";
 
 const DUMMY_PLACES = [
-    {
-        id: 'p1',
-        title: 'Empire State Building',
-        description: 'One of the most famous buildings in the world',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_Cropped.jpg/220px-View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_Cropped.jpg',
-        address: '20 W 34th St, New York, NY 10001',
-        location: {
-            lat: 40.7484405,
-            lng: -73.9878584,
-        },
-        creator: 'u1',
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous buildings in the world",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_Cropped.jpg/220px-View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_Cropped.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
     },
-    {
-        id: 'p2',
-        title: 'Empire State Building',
-        description: 'One of the most famous buildings in the world',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_Cropped.jpg/220px-View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_Cropped.jpg',
-        address: '20 W 34th St, New York, NY 10001',
-        location: {
-            lat: 40.7484405,
-            lng: -73.9878584,
-        },
-        creator: 'u2',
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Empire State Building",
+    description: "One of the most famous buildings in the world",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_Cropped.jpg/220px-View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_Cropped.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
     },
-]
+    creator: "u2",
+  },
+];
 
 const UserPlaces = () => {
-  return (
-    <PlaceList items={DUMMY_PLACES} />
-  )
-}
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
 
-export default UserPlaces
+  return <PlaceList items={loadedPlaces} />;
+};
+
+export default UserPlaces;
