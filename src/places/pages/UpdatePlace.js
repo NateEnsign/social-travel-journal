d10a6cf2e7bb97530181225bdd96133157b0d535
Nate Ensign <dixienate@gmail.com>
2025-01-07 17:27:57 -0700

    fixed the warning for when a place in params does not exist

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -7,6 +7,7 @@ import {
 } from "../../shared/util/validators";
 import Input from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
+import Card from "../../shared/components/UIElements/Card";
 import { useForm } from "../../shared/hooks/form-hook";
 import "./PlaceForm.css";
 
@@ -26,7 +27,7 @@ const DUMMY_PLACES = [
   },
   {
     id: "p2",
-    title: "Empire State Building",
+    title: "Emp. State Building",
     description: "One of the most famous buildings in the world",
     imageUrl:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_Cropped.jpg/220px-View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_Cropped.jpg",
@@ -40,8 +41,8 @@ const DUMMY_PLACES = [
 ];
 
 const UpdatePlace = () => {
-    const [isLoading, setIsLoading] = useState(true)
-        
+  const [isLoading, setIsLoading] = useState(true);
+
   const placeId = useParams().placeId;
 
   const [formState, inputHandler, setFormData] = useForm(
@@ -61,20 +62,23 @@ const UpdatePlace = () => {
   const identifiedPlace = DUMMY_PLACES.find((p) => p.id === placeId);
 
   useEffect(() => {
-    setFormData(
-      {
-        title: {
-          value: identifiedPlace.title,
-          isValid: true,
+    if (identifiedPlace) {
+      setFormData(
+        {
+          title: {
+            value: identifiedPlace.title,
+            isValid: true,
+          },
+          description: {
+            value: identifiedPlace.description,
+            isValid: true,
+          },
         },
-        description: {
-          value: identifiedPlace.description,
-          isValid: true,
-        },
-      },
-      true
-    );
-    setIsLoading(false)
+        true
+      );
+    }
+
+    setIsLoading(false);
   }, [setFormData, identifiedPlace]);
 
   const placeUpdateSubmitHandler = (event) => {
@@ -85,46 +89,48 @@ const UpdatePlace = () => {
   if (!identifiedPlace) {
     return (
       <div className="center">
-        <h2>Could not find place!</h2>
+        <Card>
+          <h2>Could not find place!</h2>
+        </Card>
       </div>
     );
   }
 
   if (isLoading) {
     return (
-        <div className="center">
-            <h2>Loading...</h2>
-        </div>
-    )
+      <div className="center">
+        <h2>Loading...</h2>
+      </div>
+    );
   }
 
   return (
-      <form className="place-form" onSubmit={placeUpdateSubmitHandler}>
-        <Input
-          id="title"
-          element="input"
-          type="text"
-          label="title"
-          validators={[VALIDATOR_REQUIRE()]}
-          errorText="Please enter a valid title."
-          onInput={inputHandler}
-          initialValue={formState.inputs.title.value}
-          initialValid={formState.inputs.title.isValid}
-        />
-        <Input
-          id="description"
-          element="textarea"
-          label="Description"
-          validators={[VALIDATOR_MINLENGTH(5)]}
-          errorText="Please enter a valid description (min 5 characters)."
-          onInput={inputHandler}
-          initialValue={formState.inputs.description.value}
-          initialValid={formState.inputs.description.isValid}
-        />
-        <Button type="submit" disabled={!formState.isValid}>
-          UPDATE PLACE
-        </Button>
-      </form>
+    <form className="place-form" onSubmit={placeUpdateSubmitHandler}>
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title."
+        onInput={inputHandler}
+        initialValue={formState.inputs.title.value}
+        initialValid={formState.inputs.title.isValid}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (min 5 characters)."
+        onInput={inputHandler}
+        initialValue={formState.inputs.description.value}
+        initialValid={formState.inputs.description.isValid}
+      />
+      <Button type="submit" disabled={!formState.isValid}>
+        UPDATE PLACE
+      </Button>
+    </form>
   );
 };
 
--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -5,32 +5,32 @@ import PlaceList from "../components/PlaceList";
 
 
 const DUMMY_PLACES = [
-  // {
-  //   id: "p1",
-  //   title: "Empire State Building",
-  //   description: "One of the most famous buildings in the world",
-  //   imageUrl:
-  //     "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_Cropped.jpg/220px-View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_Cropped.jpg",
-  //   address: "20 W 34th St, New York, NY 10001",
-  //   location: {
-  //     lat: 40.7484405,
-  //     lng: -73.9878584,
-  //   },
-  //   creator: "u1",
-  // },
-  // {
-  //   id: "p2",
-  //   title: "Empire State Building",
-  //   description: "One of the most famous buildings in the world",
-  //   imageUrl:
-  //     "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_Cropped.jpg/220px-View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_Cropped.jpg",
-  //   address: "20 W 34th St, New York, NY 10001",
-  //   location: {
-  //     lat: 40.7484405,
-  //     lng: -73.9878584,
-  //   },
-  //   creator: "u2",
-  // },
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous buildings in the world",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_Cropped.jpg/220px-View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_Cropped.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Emp. State Building",
+    description: "One of the most famous buildings in the world",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_Cropped.jpg/220px-View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_Cropped.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: "u2",
+  },
 ];
 
 const UserPlaces = () => {
