0ac7b79ec9301dded8ac78cd41516e27d7e6e03b
Nate Ensign <dixienate@gmail.com>
2025-02-05 20:20:15 -0700

    built a custom http hook to handle all http calls throughout my app

---
+++ src/shared/hooks/http-hook.js
@@ -0,0 +1,51 @@
+import { useState, useCallback, useRef, useEffect } from "react";
+
+export const useHttpClient = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+
+  const activeHttpRequests = useRef([])
+
+  const sendRequest = useCallback(async (
+    url,
+    method = "GET",
+    body = null,
+    headers = {}
+  ) => {
+    setIsLoading(true);
+    const httpAbortCtrll = new AbortController();
+    activeHttpRequests.current.push(httpAbortCtrll);
+
+    try {
+      fetch(url, {
+        method,
+        body,
+        headers,
+        signal: httpAbortCtrll.signal
+      });
+
+      const responseData = await response.json();
+
+      if (!response.ok) {
+        throw new Error(responseData.message);
+      }
+
+      return responseData;
+    } catch (err) {
+      setError(err.message);
+    }
+    setIsLoading(false);
+  }, []);
+
+  const clearError = () => {
+    setError(null);
+  }
+
+  useEffect(() => {
+    return () => {
+        activeHttpRequests.current.forEach(abortCtrl.abort());
+    }
+  }, [])
+
+  return { isLoading, error, sendRequest, clearError }
+};
