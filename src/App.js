3e74ba40ce6a9e84f3c23e7fd662f75b9f567a5a
Nate Ensign <dixienate@gmail.com>
2025-03-02 13:17:27 -0700

    fixed small bug

--- src/App.js
+++ src/App.js
@@ -1,67 +1,22 @@
-import React, { useState, useCallback, useEffect } from "react";
+import React from 'react';
 import {
   BrowserRouter as Router,
   Route,
   Redirect,
-  Switch,
-} from "react-router-dom/cjs/react-router-dom.min";
-import Users from "./user/pages/Users";
-import NewPlace from "./places/pages/NewPlace";
-import UserPlaces from "./places/pages/UserPlaces";
-import UpdatePlace from "./places/pages/UpdatePlace";
-import MainNavigation from "./shared/components/Navigation/MainNavigation";
-import Auth from "./user/pages/Auth";
-import { AuthContext } from "./shared/context/auth-context";
+  Switch
+} from 'react-router-dom';
 
-let logoutTimer;
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+import UserPlaces from './places/pages/UserPlaces';
+import UpdatePlace from './places/pages/UpdatePlace';
+import Auth from './user/pages/Auth';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
+import { AuthContext } from './shared/context/auth-context';
+import { useAuth } from './shared/hooks/auth-hook';
 
 const App = () => {
-  const [token, setToken] = useState(false);
-  const [userId, setUserId] = useState(null);
-  const [tokenExpirationDate, setTokenExpirationDate] = useState();
-
-  const login = useCallback((uId, token, expirationDate) => {
-    setToken(token);
-    setUserId(uId);
-    const tokenExpirationDate =
-      expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
-      setTokenExpirationDate(tokenExpirationDate);
-    localStorage.setItem(
-      "userData",
-      JSON.stringify({
-        userId: uId,
-        token: token,
-        expiration: tokenExpirationDate.toISOString(),
-      })
-    );
-  }, []);
-
-  const logout = useCallback(() => {
-    setToken(null);
-    setTokenExpirationDate(null);
-    setUserId(null);
-    localStorage.removeItem("userData");
-  }, []);
-
-  useEffect(() => {
-    if (token && tokenExpirationDate){
-      const remainingTime = tokenExpirationDate.getTime() - new Date().getTime();
-      logoutTimer = setTimeout(logout, remainingTime);
-    }else {
-      clearTimeout(logoutTimer);
-    }
-  }, [token, logout, tokenExpirationDate])
-
-  useEffect(() => {
-    const storedData = JSON.parse(localStorage.getItem("userData"));
-    if (
-      storedData &&
-      storedData.token &&
-      new Date(storedData.expiration) > new Date()
-    ) {
-      login(storedData.userId, storedData.token, new Date(storedData.expiration));
-    }
-  }, [login]);
+  const { token, login, logout, userId } = useAuth();
 
   let routes;
 
@@ -71,8 +26,8 @@ const App = () => {
         <Route path="/" exact>
           <Users />
         </Route>
-        <Route path="/:userId/places">
-          <UserPlaces exact />
+        <Route path="/:userId/places" exact>
+          <UserPlaces />
         </Route>
         <Route path="/places/new" exact>
           <NewPlace />
@@ -89,8 +44,8 @@ const App = () => {
         <Route path="/" exact>
           <Users />
         </Route>
-        <Route path="/:userId/places">
-          <UserPlaces exact />
+        <Route path="/:userId/places" exact>
+          <UserPlaces />
         </Route>
         <Route path="/auth">
           <Auth />
@@ -107,7 +62,7 @@ const App = () => {
         token: token,
         userId: userId,
         login: login,
-        logout: logout,
+        logout: logout
       }}
     >
       <Router>
--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -1,20 +1,20 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext } from 'react';
 
-import Card from "../../shared/components/UIElements/Card";
-import Input from "../../shared/components/FormElements/Input";
-import Button from "../../shared/components/FormElements/Button";
-import ErrorModal from "../../shared/components/UIElements/ErrorModal";
-import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
-import ImageUpload from "../../shared/components/FormElements/ImageUpload";
+import Card from '../../shared/components/UIElements/Card';
+import Input from '../../shared/components/FormElements/Input';
+import Button from '../../shared/components/FormElements/Button';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+import ImageUpload from '../../shared/components/FormElements/ImageUpload';
 import {
   VALIDATOR_EMAIL,
   VALIDATOR_MINLENGTH,
-  VALIDATOR_REQUIRE,
-} from "../../shared/util/validators";
-import { useForm } from "../../shared/hooks/form-hook";
-import { useHttpClient } from "../../shared/hooks/http-hook";
-import { AuthContext } from "../../shared/context/auth-context";
-import "./Auth.css";
+  VALIDATOR_REQUIRE
+} from '../../shared/util/validators';
+import { useForm } from '../../shared/hooks/form-hook';
+import { useHttpClient } from '../../shared/hooks/http-hook';
+import { AuthContext } from '../../shared/context/auth-context';
+import './Auth.css';
 
 const Auth = () => {
   const auth = useContext(AuthContext);
@@ -24,13 +24,13 @@ const Auth = () => {
   const [formState, inputHandler, setFormData] = useForm(
     {
       email: {
-        value: "",
-        isValid: false,
+        value: '',
+        isValid: false
       },
       password: {
-        value: "",
-        isValid: false,
-      },
+        value: '',
+        isValid: false
+      }
     },
     false
   );
@@ -50,8 +50,8 @@ const Auth = () => {
         {
           ...formState.inputs,
           name: {
-            value: "",
-            isValid: false,
+            value: '',
+            isValid: false
           },
           image: {
             value: null,
@@ -61,44 +61,40 @@ const Auth = () => {
         false
       );
     }
-
-    setIsLoginMode((prevMode) => !prevMode);
+    setIsLoginMode(prevMode => !prevMode);
   };
 
-  const authSubmitHandler = async (event) => {
+  const authSubmitHandler = async event => {
     event.preventDefault();
-
-    console.log(formState.inputs);
 
     if (isLoginMode) {
       try {
         const responseData = await sendRequest(
-          "http://localhost:5000/api/users/login",
-          "POST",
+          'http://localhost:5000/api/users/login',
+          'POST',
           JSON.stringify({
             email: formState.inputs.email.value,
-            password: formState.inputs.password.value,
+            password: formState.inputs.password.value
           }),
           {
-            "Content-Type": "application/json",
+            'Content-Type': 'application/json'
           }
         );
         auth.login(responseData.userId, responseData.token);
-      } catch (err) {
-        console.log('Login error', err);
-      }
+      } catch (err) {}
     } else {
       try {
         const formData = new FormData();
-        formData.append('email', formState.inputs.email.value)
-        formData.append('name', formState.inputs.name.value)
-        formData.append('password', formState.inputs.password.value)
-        formData.append('image', formState.inputs.image.value)
+        formData.append('email', formState.inputs.email.value);
+        formData.append('name', formState.inputs.name.value);
+        formData.append('password', formState.inputs.password.value);
+        formData.append('image', formState.inputs.image.value);
         const responseData = await sendRequest(
-          "http://localhost:5000/api/users/signup",
-          "POST",
+          'http://localhost:5000/api/users/signup',
+          'POST',
           formData
         );
+
         auth.login(responseData.userId, responseData.token);
       } catch (err) {}
     }
@@ -118,19 +114,26 @@ const Auth = () => {
               id="name"
               type="text"
               label="Your Name"
-              validators={[VALIDATOR_REQUIRE]}
+              validators={[VALIDATOR_REQUIRE()]}
               errorText="Please enter a name."
               onInput={inputHandler}
             />
           )}
-          {!isLoginMode && <ImageUpload id='image' onInput={inputHandler} center />}
+          {!isLoginMode && (
+            <ImageUpload
+              center
+              id="image"
+              onInput={inputHandler}
+              errorText="Please provide an image."
+            />
+          )}
           <Input
             element="input"
             id="email"
             type="email"
-            label="Email"
+            label="E-Mail"
             validators={[VALIDATOR_EMAIL()]}
-            errorText="Please eneter a valid email address."
+            errorText="Please enter a valid email address."
             onInput={inputHandler}
           />
           <Input
@@ -139,15 +142,15 @@ const Auth = () => {
             type="password"
             label="Password"
             validators={[VALIDATOR_MINLENGTH(6)]}
-            errorText="Please eneter a valid password (at least 5 characters)."
+            errorText="Please enter a valid password, at least 6 characters."
             onInput={inputHandler}
           />
           <Button type="submit" disabled={!formState.isValid}>
-            {isLoginMode ? "LOGIN" : "SIGN UP"}
+            {isLoginMode ? 'LOGIN' : 'SIGNUP'}
           </Button>
         </form>
         <Button inverse onClick={switchModeHandler}>
-          SWITCH TO {isLoginMode ? "SIGN UP" : "LOGIN"}
+          SWITCH TO {isLoginMode ? 'SIGNUP' : 'LOGIN'}
         </Button>
       </Card>
     </React.Fragment>
